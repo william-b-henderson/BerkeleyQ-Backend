e3f472566a450822b5536fe39dba5b03603b3d1b
Bhavesh Kalisetti <bhaveshkalisetti@Bhaveshs-MacBook-Pro.local>
2022-10-15 18:36:15 -0700

    basic routes

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/classes')
+def get_classes():
+    return "Classes"
+
+@app.route('/schedule/<class_id>')
+def get_class_schedule(class_id):
+    return "Class schedule for class_id {}".format(class_id)
+
+@app.route('/oh/<class_id>/<oh_id>')
+def get_class_oh(class_id, oh_id):
+    return "OH {} for class {}".format(oh_id, class_id)
